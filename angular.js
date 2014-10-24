e32c2602da3136a36a5d6ef5f807babaa5211eb2
euniceroh <bendthepen@gmail.com>
2014-10-24 10:18:48 -0700

    initial upload

---
+++ angular.js
@@ -0,0 +1,35 @@
+var app = angular.module("ideaboard", ["firebase"]);
+
+app.controller("ideaCtrl", function($scope, $firebase){
+	var x = new Firebase("https://emriboard.firebaseio.com/");
+	var sync = $firebase(x);
+	$scope.posts = sync.$asArray();
+
+	$scope.addPin = function (a, b, c, d, e){
+		$scope.posts.$add({image_url: a, video: b, audio: c, words: d, hashtags: e});
+		$scope.image_url="";
+		$scope.video="";
+		$scope.audio="";
+		$scope.words="";
+		$scope.hashtags="";
+	}
+	
+	$scope.deletePin = function (entry){
+		var check= confirm("are you sure you want to delete?");
+		if (check == true){
+			$scope.posts.$remove(entry);
+		}
+	}
+/////////////////////////////////////////////////////////
+	$(document).ready(function(){
+	  $("#addbutton").click(function(){
+	    $("#formpanel").slideDown("slow");
+	  });
+	});
+	$(document).ready(function(){
+	  $("#doneadd").click(function(){
+	    $("#formpanel").slideUp("slow");
+	  });
+	});
+
+})
