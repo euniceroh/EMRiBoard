40590e22d723b98d60ebed1e3e94900a8732cbe0
euniceroh <bendthepen@gmail.com>
2014-10-26 22:02:24 -0700

    added like button

--- angular.js
+++ angular.js
@@ -5,22 +5,33 @@ app.controller("ideaCtrl", function($scope, $firebase){
 	var sync = $firebase(x);
 	$scope.posts = sync.$asArray();
 
+/////////////////////////////////////////////////////////NG REPEAT
 	$scope.addPin = function (a, b, c, d, e){
-		$scope.posts.$add({image_url: a, video: b, audio: c, words: d, hashtags: e});
+		$scope.posts.$add({image_url: a, video: b, audio: c, words: d, hashtags: e, like: false});
 		$scope.image_url="";
 		$scope.video="";
 		$scope.audio="";
 		$scope.words="";
 		$scope.hashtags="";
 	}
-	
+
+/////////////////////////////////////////////////////////ADD & DELETE
 	$scope.deletePin = function (entry){
 		var check= confirm("are you sure you want to delete?");
 		if (check == true){
 			$scope.posts.$remove(entry);
 		}
 	}
-/////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////LIKE
+	$scope.likePin = function(entry){
+		entry.like = (!entry.like);
+
+		$scope.posts.$save(entry);
+	}
+
+
+/////////////////////////////////////////////////////////ADD TOGGLE
 	$(document).ready(function(){
 	  $("#addbutton").click(function(){
 	    $("#formpanel").slideDown("slow");
@@ -31,15 +42,15 @@ app.controller("ideaCtrl", function($scope, $firebase){
 	    $("#formpanel").slideUp("slow");
 	  });
 	});
-})
 
-//////////////////////////////////////////////////////////
 
-// 	$(window).load(function(){
-// 		$container.masonry({
-// 		  // columnWidth: 25,
-// 		  // itemSelector: '.columns',
-// 	})
-// });
+
+});
+
+
+
+
+
+
 
 
